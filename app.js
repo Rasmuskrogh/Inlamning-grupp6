594d3a78d0da0efa3384ce4cef3b6539e8c9e78f
Olle Nilsson <[olle.nilsson@medieinstitutet.se]>
2020-12-07 16:21:32 +0100

    function for pushing items to shoppingcart array

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let balance = 0;
 
 //För delete och edit knappar 
 
-const DELETE = "delete", EDIT = "edit";
+const DELETE = "delete", EDIT = "edit", ADDTOCART = "addCartBtn";
 
 // Kollar om det finns sparad data i localstorage
 
@@ -49,18 +49,20 @@ function deleteOrEdit(event){
   const targetBtn = event.target;
 
   const product = targetBtn.parentNode;
-
+  
   if ( targetBtn.id == DELETE ){
     deleteProduct(product);
 
   }else if(targetBtn.id == EDIT ){
     editProduct(product);
+  }else if(targetBtn.id == ADDTOCART ){
+    addToCart(product);
   }
 
 }
 
 
-// delete och edit functions
+// delete och edit och addToCart functions
 
 
 //delete function för att tabort rätt product i arrayen, väljer efter id
@@ -79,6 +81,13 @@ function editProduct(product){
   addPrice.value = PRODUCT.price;
 
   deleteProduct(product);
+}
+
+//Buy product function för att pusha object till SHOPPING_CART
+
+function addToCart(product){
+  SHOPPING_CART.push(product);
+  console.log(SHOPPING_CART);
 }
 
 function updateUI(){
@@ -147,7 +156,75 @@ function clearInput(inputs){
   })
 }
 
+// För varukorgen
 
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoading", ready)
+}
+else {
+  ready()
+}
+
+function ready() {
+  const deletebtn = document.getElementsByClassName("delete")
+  for (var i = 0; i <deletebtn.length; i++) {
+      let button = deletebtn[i]
+      button.addEventListener("click", removeItem )
+  }
+
+var quantityInput = document.getElementsByClassName("item-quantity")
+for (var i = 0; i <quantityInput.length; i++) {
+    var input = quantityInput[i]
+    input.addEventListener("change", quantityChange)
+}
+
+var addToCartBtn = document.getElementById("addCartBtn") 
+for (var i = 0; i <addToCartBtn.length; i++) {
+    var button = addToCartBtn[i]
+    button.addEventListener("click", addToCartClicked)  
+}
+} 
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+function removeItem(event) {
+  var btnClicked = event.target
+  btnClicked.parentElement.parentElement.remove()
+  updateTotal()
+}
+
+function quantityChange(event) {
+  var input = event.target
+  if(isNaN(input.value) || input.value <= 0) {
+      input.value = 1
+  }
+  updateTotal()
+}
+
+function addToCartClicked(event) {
+  var button = event.target
+  var shopItem = button.parentElement
+  var title = shopItem.getElementsByClassName("product_title")[0].innerText
+  console.log(title);
+}
+
+function updateTotal() {
+  var dropDownContent = document.getElementsByClassName("dropdown-content")[0]
+   var cartItems = dropDownContent.getElementsByClassName("cart-item")
+   var total = 0
+   for (var i = 0; i <cartItems.length; i++) {
+      var cartItem = cartItems[i] 
+      var priceElement = cartItem.getElementsByClassName("item-price")[0]
+      var quantityElement = cartItem.getElementsByClassName("item-quantity")[0]
+      var price = parseFloat(priceElement.innerText.replace("kr", ""))
+      var quantity = quantityElement.value
+      total = total + (price * quantity)
+  } 
+   document.getElementsByClassName("total-price")[0].innerText = total + "kr"
+} 
 
 
 
